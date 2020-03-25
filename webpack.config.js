ab03e94863b25a038fa12c55b4d093009fc563f7
Ángel Calderón <angel@acalderon.local>
2020-03-25 20:31:51 -0300

    add webpack

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+const nodemonPlugin = require('nodemon-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.ts',
+    target: 'node',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index.js'
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: [
+                    'ts-loader'
+                ]
+            }
+        ]
+    },
+    externals: [ nodeExternals() ],
+    plugins: [
+        new nodemonPlugin()
+    ]
+}
